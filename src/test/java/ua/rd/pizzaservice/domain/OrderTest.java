32c64f943957a40cc9beec03c426afdb970f54ba
tecbak <oveile55>
2016-10-29 18:32:01 +0300

    add tests for class Oder

--- src/test/java/ua/rd/pizzaservice/domain/OrderTest.java
+++ src/test/java/ua/rd/pizzaservice/domain/OrderTest.java
@@ -3,6 +3,11 @@ package ua.rd.pizzaservice.domain;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import ua.rd.pizzaservice.domain.discount.Discount;
 import ua.rd.pizzaservice.domain.order.Order;
 import ua.rd.pizzaservice.domain.pizza.Pizza;
 import ua.rd.pizzaservice.domain.pizza.Type;
@@ -12,78 +17,90 @@ import ua.rd.pizzaservice.repository.PizzaRepository;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import static java.math.BigDecimal.*;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class OrderTest {
     private Order order;
-    private PizzaRepository pizzaRepository;
-    private Pizza vegeterian;
+    @Mock
+    private Pizza vegetarian;
+    @Mock
     private Pizza meat;
+    @Mock
     private Pizza sea;
     private HashMap<Pizza, Integer> pizzas;
+    @Mock
     private Customer customer;
+    @Mock
+    private Discount discount;
 
 
     @Before
     public void setUp() throws Exception {
-        pizzaRepository = new InMemPizzaRepository();
-        vegeterian = new Pizza(1L, "Vegetarian", BigDecimal.valueOf(60), Type.VEGETARIAN);
-        meat = new Pizza(2L, "Meat", BigDecimal.valueOf(80), Type.MEAT);
-        sea = new Pizza(3L, "Sea", BigDecimal.valueOf(100), Type.SEA);
+
+        MockitoAnnotations.initMocks(this);
+        when(vegetarian.getPrice()).thenReturn(valueOf(60.0));
+        when(meat.getPrice()).thenReturn(valueOf(80.0));
+        when(sea.getPrice()).thenReturn(valueOf(100.0));
+
         pizzas = new HashMap<Pizza, Integer>() {{
-            put(vegeterian, 2);
+            put(vegetarian, 2);
             put(meat, 2);
             put(sea, 1);
-//            add(vegeterian);
-//            add(vegeterian);
-//            add(meat);
-//            add(meat);
-//            add(sea);
         }};
-        customer = new Customer();
         order = new Order(customer, pizzas);
+        when(discount.calculate(eq(order), any(BigDecimal.class))).thenReturn(valueOf(10.0));
     }
 
     @Test
     public void testGetPrice() throws Exception {
         BigDecimal expected = sea.getPrice(). //multiply(BigDecimal.valueOf(0.7)).
-                add(vegeterian.getPrice().add(vegeterian.getPrice()).
+                add(vegetarian.getPrice().add(vegetarian.getPrice()).
                 add(meat.getPrice()).add(meat.getPrice()));
         BigDecimal actual = order.getPrice();
 
         assertEquals(expected, actual);
     }
 
-    @Ignore
     @Test
-    public void getDiscountTest() throws Exception {
-//        BigDecimal expected = sea.getPrice().multiply(BigDecimal.valueOf(0.3));
-//        BigDecimal actual = order.getQuantityDiscount();
-//
-//        assertEquals(expected, actual);
+    public void testSize() throws Exception {
+        int expected = 5;
+        int actual = order.size();
+        assertEquals(expected, actual);
     }
 
-    @Ignore
     @Test
-    public void setStatusTest() throws Exception {
-//        order.setStatus(Status.IN_PROGRESS);
-//        order.setStatus(Status.DONE);
+    public void testApplyDiscountPrice() throws Exception {
+        BigDecimal expected = valueOf(10.0);
+        BigDecimal actual = order.applyDiscount(discount);
+
+        assertEquals(expected, actual);
     }
 
-    @Ignore
-    @Test(expected = RuntimeException.class)
-    public void onSettingStatusInProgressAfterStatusCancelled_throwException() throws Exception {
-//        order.setStatus(Status.CANCELLED);
-//        order.setStatus(Status.IN_PROGRESS);
+    @Test
+    public void testGetDiscountedPrice() throws Exception {
+        order.applyDiscount(discount);
+
+        BigDecimal expected = valueOf(370.0);
+        BigDecimal actual = order.getDiscountedPrice();
+
+        assertEquals(expected, actual);
     }
 
-    @Ignore
     @Test
-    public void getPriceWithDiscountsTest() {
-//        customer.depositToLoyaltyCard(BigDecimal.valueOf(2000));
-//
-//        System.out.println(order.getTotalDiscount());
-//        System.out.println(order.getLoyaltyCardDiscount());
-//        System.out.println(order.getQuantityDiscount());
+    public void testPay() throws Exception {
+        order.pay();
+        verify(customer).depositToLoyaltyCard(order.getDiscountedPrice());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIfPayInvokedAgain_throwException() throws Exception {
+        order.pay();
+        order.pay();
     }
 }
