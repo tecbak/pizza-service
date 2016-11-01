4bc64034fe8168204c0195462e3feca667f01325
tecbak <oveile55>
2016-11-02 00:18:43 +0200

    start to do SimpleCustomerServiceIT.testSave()

--- src/test/java/ua/rd/pizzaservice/services/SimpleCustomerServiceIT.java
+++ src/test/java/ua/rd/pizzaservice/services/SimpleCustomerServiceIT.java
@@ -1,44 +1,59 @@
 package ua.rd.pizzaservice.services;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
+import org.springframework.jdbc.core.RowMapper;
 import ua.rd.pizzaservice.domain.customer.Address;
 import ua.rd.pizzaservice.domain.customer.Customer;
+import ua.rd.pizzaservice.repository.CustomerRepository;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import static java.math.BigDecimal.*;
 
 
 public class SimpleCustomerServiceIT extends ServiceTestConfig {
+    private static class CustomerRowMapper implements RowMapper {
+        @Override
+        public Object mapRow(ResultSet resultSet, int i) throws SQLException {
+            throw new UnsupportedOperationException();
+        }
+    }
+
     @Autowired
     private CustomerService customerService;
+    private Address address0;
+    private Customer customer0;
+    private Address address1;
+    private Customer customer1;
 
-    @Test
-    public void testFindAll() throws Exception {
-        Address address0 = new Address();
+    @Before
+    public void setUp() throws Exception {
+        address0 = new Address();
         address0.setCity("Warsaw");
         address0.setStreet("Mickiewicz str");
         address0.setBuilding("10A");
-        Customer customer0 = new Customer();
+        customer0 = new Customer();
         customer0.setName("Adam");
         customer0.setAddress(address0);
         customer0.depositToLoyaltyCard(valueOf(100.00));
 
-        Address address1 = new Address();
+        address1 = new Address();
         address1.setCity("Roma");
         address1.setStreet("Via Torino");
         address1.setBuilding("15");
-        Customer customer1 = new Customer();
+        customer1 = new Customer();
         customer1.setName("Mary");
         customer1.setAddress(address1);
         customer1.depositToLoyaltyCard(valueOf(200.00));
+    }
 
+    @Test
+    public void testFindAll() throws Exception {
         String insertAddress =
                 "INSERT INTO addresses (id, city, street, building) VALUES (?, ?, ?, ?)";
         jdbcTemplate.update(insertAddress,
@@ -56,5 +71,12 @@ public class SimpleCustomerServiceIT extends ServiceTestConfig {
         List<Customer> customers = customerService.findAll();
         Assert.assertEquals(customer0, customers.get(0));
         Assert.assertEquals(customer1, customers.get(1));
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        customerService.save(customer0.getName(), address0.getCity(), address0.getStreet(), address0.getBuilding());
+        String selectCustomer = "SELECT ";
+        Customer actual = (Customer) jdbcTemplate.queryForObject(selectCustomer, new Object[]{customer0.getName()}, new CustomerRowMapper());
     }
 }
